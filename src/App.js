f35172d548320bd13a82d3a16551fff3328bb415
Mehmet Nuri Cavdar <113375790+mehmetnuricavdar@users.noreply.github.com>
2023-04-18 23:57:29 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  if (action.type === "buy_ingredients") return { money: state.money - 10 };
+  if (action.type === "sell_a_meal") return { money: state.money + 10 };
+  if (action.type === "celebrity_visit") return { money: state.money + 2000 };
+  return state;
+};
+
+export default function App() {
+  const initialState = { money: 100 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div className="App">
+      <h1>Wallet: {state.money}</h1>
+      <div>
+        <button onClick={() => dispatch({ type: "buy_ingredients" })}>
+          Shopping for veggies!
+        </button>
+        <button onClick={() => dispatch({ type: "sell_a_meal" })}>
+          Serve a meal to the customer
+        </button>
+        <button onClick={() => dispatch({ type: "celebrity_visit" })}>
+          Celebrity visit
+        </button>
+      </div>
+    </div>
+  );
+}
